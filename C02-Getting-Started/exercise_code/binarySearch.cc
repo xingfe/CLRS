709b19ae89910be3392d857eaf899e7282a3840d
luoxing <luoxing@didichuxing.com>
2018-04-16 12:59:33 +0800

    push missing

---
+++ C02-Getting-Started/exercise_code/binarySearch.cc
@@ -0,0 +1,18 @@
+
+
+int binarySearch(std::vector<int>& items, int v){
+    int low = 0 ;
+    int high = items.size() -1;
+    while(low <= high){
+        int mid = (low+(high-low)/2);
+        if(items[mid] ==v ){
+            return mid;
+        }else if(items[mid] ==v){
+            low++;
+        }else{
+            high--;
+        }
+    }
+    return -1;
+}
+
---
+++ C04-Recurrences/exercise_code/findMissing.cc
@@ -0,0 +1,9 @@
+#include "findMissing.hpp"
+// assert min(vec) == 0
+// assert max(vec) == vec.size() - 1
+
+
+int main(){
+  std::vector<int> items ={0,1,2,3,5,6,7,8};
+  std::cout << findmissing(items);
+}
